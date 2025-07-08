ad714334fddf804eaef798a0c9953ec1afb8cffe
Ram Jesler Delos Santos <ramjesler.delossantos@gmail.com>
2025-07-08 19:17:42 +0800

    Added a new route

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -2,5 +2,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/index.tsx"),
-    route("/login", "routes/auth/login.tsx"),
+    route("/login", "routes/auth/login/login.tsx"),
+    route("/signup", "routes/auth/signup/signup.tsx")
 ] satisfies RouteConfig;
