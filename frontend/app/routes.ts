7116791fb58f3ca0c39e28175c16374cf398cde7
Ram Jesler Delos Santos <ramjesler.delossantos@gmail.com>
2025-07-09 18:27:01 +0800

    Added home page route

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,5 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/index.tsx"),
     route("/login", "routes/auth/login/login.tsx"),
-    route("/signup", "routes/auth/signup/signup.tsx")
+    route("/signup", "routes/auth/signup/signup.tsx"),
+    route("/home", "routes/home/home.tsx")
 ] satisfies RouteConfig;
