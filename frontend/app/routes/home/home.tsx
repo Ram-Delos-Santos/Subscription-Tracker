44636d957db67ce7dc3ad7c92a7d411e9a2a7c53
Ram-Delos-Santos <ramjesler.delossantos@gmail.com>
2025-07-24 18:18:18 +0800

    Added a new frame for home page

--- frontend/app/routes/home/home.tsx
+++ frontend/app/routes/home/home.tsx
@@ -1,8 +1,15 @@
+import logo from '../../images/logo.png'
+
 export default function HomePage() {
     return (
-        <main className="w-screen h-screen bg-[url('https://ichef.bbci.co.uk/ace/standard/2560/cpsprodpb/1a8a/live/5c76f660-5520-11f0-9ba3-792c3b1efecd.jpg')] bg-cover relative overflow-hidden">
-            <div className="items-center flex justify-center h-screen">
-                <h1 className="text-green-700 text-center font-bold text-9xl">Hello JB!</h1>
+        <main className="w-screen h-screen bg-gradient-to-b from-[#6DB1C9] to-white relative overflow-hidden">
+            <div className="w-full h-20 fixed flex items-center top-0 left-0 bg-white text-[#74CFDE] shadow-md z-50">
+                <div className="max-w-7xl ml-5 mr-auto flex items-center justify-between px-2 py-3">
+          <div className='flex items-center space-x-3'>
+            <img src={logo} alt="Logo" className="h-10 w-10" />
+            <span className='text-3xl font-semibold'>SubPiece</span>
+          </div>
+        </div>
             </div>
         </main>
     )
